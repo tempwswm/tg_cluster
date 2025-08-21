a78e14a6f09febab6f0a7796c8328e42c61cc4b6
我是哇民 <126407303+tempwswm@users.noreply.github.com>
2025-08-21 19:28:24 +0800

    Update build.sh

--- tg-novnc/build.sh
+++ tg-novnc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export BUILD_IMG_TAG="registry.cluster.local/tg-novnc:12_1.1.77_v3" &&\
+export BUILD_IMG_TAG="registry.cluster.local/tg-novnc:12_1.1.77_v4" &&\
 podman build -t $BUILD_IMG_TAG . &&\
 podman save -o image.tar $BUILD_IMG_TAG &&\
 k3s ctr -n k8s.io i import image.tar &&\
