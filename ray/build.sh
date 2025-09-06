4730f48d02f5879f9b0aff1b11c7781794b04ebf
tempwswm <tempwswm@protonmail.com>
2025-09-06 17:14:52 +0800

    添加uv

--- ray/build.sh
+++ ray/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export BUILD_IMG_TAG="registry.cluster.local/ray:2.49.0" &&\
+export BUILD_IMG_TAG="registry.cluster.local/ray:2.49.0_v2" &&\
 podman build -t $BUILD_IMG_TAG . &&\
 podman save -o image.tar $BUILD_IMG_TAG &&\
 k3s ctr -n k8s.io i import image.tar &&\
