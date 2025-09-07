ffa54538319d8f21c613263715acdfb7757fe6ac
tempwswm <tempwswm@protonmail.com>
2025-09-07 23:12:48 +0800

    更新ray,添加时区，更新64gram

--- ray/build.sh
+++ ray/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export BUILD_IMG_TAG="registry.cluster.local/ray:2.49.0_v2" &&\
-podman build -t $BUILD_IMG_TAG . &&\
+export RAY_VERSION="2.49.1" &&\
+export BUILD_IMG_TAG="registry.cluster.local/ray:${RAY_VERSION}" &&\
+podman build -t $BUILD_IMG_TAG . --build-arg RAY_VERSION=$RAY_VERSION &&\
 podman save -o image.tar $BUILD_IMG_TAG &&\
 k3s ctr -n k8s.io i import image.tar &&\
 rm image.tar
--- tg-novnc/build.sh
+++ tg-novnc/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export BUILD_IMG_TAG="registry.cluster.local/tg-novnc:12_1.1.77_v4" &&\
-podman build -t $BUILD_IMG_TAG . &&\
+export TG_VERSION="1.1.78" &&\
+export BUILD_IMG_TAG="registry.cluster.local/tg-novnc:12_${TG_VERSION}" &&\
+podman build -t $BUILD_IMG_TAG . --build-arg TG_VERSION=$TG_VERSION &&\
 podman save -o image.tar $BUILD_IMG_TAG &&\
 k3s ctr -n k8s.io i import image.tar &&\
 rm image.tar
